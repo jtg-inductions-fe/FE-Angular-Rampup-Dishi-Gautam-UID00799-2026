07f555aaabbdbeef40fc84dd67bb669513cc0610
dishi <dishi.gautam@joshtechnologygroup.com>
2026-09-16 14:50:01 +0530

    chore: empty main branch

--- frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import {NotFoundComponent} from './shared/components/not-found/not-found.component'
-
-export const routes: Routes = [{
-    path:"**",
-    component:NotFoundComponent,
-}];
