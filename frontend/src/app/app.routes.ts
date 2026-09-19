5b8053baf5096ef533ed7abd5642bebc02ffcad8
dishi <dishi.gautam@joshtechnologygroup.com>
2026-09-19 12:31:37 +0530

    [DG_A2_01]: Login and Signup pages add and functionality

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './modules/home-page/home-page.component';
-import { NotFoundComponent } from './shared/components/not-found/not-found.component';
-import { authGuard } from './core/guards/auth.guards';
 import { authRoutes } from './modules/auth/auth.routes';
+import { authGuard } from './core/guards/auth.guards';
+import { HomePageComponent } from './modules/home-page/home-page.component';
+import {NotFoundComponent} from './shared/components/not-found/not-found.component'
 
 export const routes: Routes = [
     {
