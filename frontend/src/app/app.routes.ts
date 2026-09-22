299ae7686338a3181b80cb684e687453c45caae3
dishi <dishi.gautam@joshtechnologygroup.com>
2026-09-22 23:38:33 +0530

    [DG_A2_01]:Auth routes variable fix

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { AUTH_ROUTES } from './modules/auth/auth.routes';
 import { HomePageComponent } from './modules/home-page/home-page.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { authGuard } from './core/guards/auth.guards';
+import { authRoutes } from './modules/auth/auth.routes';
 
 export const routes: Routes = [
     {
@@ -10,7 +10,7 @@ export const routes: Routes = [
         component: HomePageComponent,
         canActivate:[authGuard]
     },
-    ...AUTH_ROUTES,
+    ...authRoutes,
     {
         path: '**',
         component: NotFoundComponent,
