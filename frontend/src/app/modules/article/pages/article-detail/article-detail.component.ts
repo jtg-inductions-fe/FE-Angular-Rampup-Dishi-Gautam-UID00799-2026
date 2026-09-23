e3860d25c8d3b54bd67cc24d4e37590ddfc446f8
dishi <dishi.gautam@joshtechnologygroup.com>
2026-09-23 15:08:05 +0530

    [DG_A2_03]: Article detail styling update

--- frontend/src/app/modules/article/pages/article-detail/article-detail.component.ts
+++ frontend/src/app/modules/article/pages/article-detail/article-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, signal, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Article } from '@app/core/models/article.model';
-import { ArticleService } from '@app/core/services/article.service';
 import { DatePipe } from '@angular/common';
 import { Location } from '@angular/common';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Article } from '@app/core/models/article.model';
+import { ArticleService } from '@app/core/services/article.service';
 
 @Component({
     selector: 'app-article-detail',
@@ -12,11 +13,13 @@ import { Location } from '@angular/common';
     templateUrl: './article-detail.component.html',
     styleUrl: './article-detail.component.scss',
 })
-export class ArticleDetailComponent {
-    article = signal<Article | null>(null);
-  private readonly location = inject(Location);
+export class ArticleDetailComponent implements OnInit {
+    protected readonly article = signal<Article | null>(null);
+
+    private readonly location = inject(Location);
     private readonly route = inject(ActivatedRoute);
     private readonly articleService = inject(ArticleService);
+
     ngOnInit(): void {
         const id = this.route.snapshot.paramMap.get('id');
 
@@ -30,7 +33,8 @@ export class ArticleDetailComponent {
             },
         });
     }
+
     goBack(): void {
-    this.location.back();
-}
+        this.location.back();
+    }
 }
